51076398999d595b5162eed4b4dcf94abde05e5d
Markus Wüstenberg <markus@wustenberg.dk>
2019-02-22 13:03:34 +0100

    Add Slowdown middleare (#3)
    Useful for development.

---
+++ slowdown_test.go
@@ -0,0 +1,31 @@
+package middleware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"middleware"
+)
+
+func TestSlowdown(t *testing.T) {
+	t.Run("slows down handling", func(t *testing.T) {
+		done := make(chan bool, 1)
+
+		h := middleware.Add(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			done <- true
+		}), middleware.Slowdown(time.Second))
+
+		go func() {
+			h.ServeHTTP(httptest.NewRecorder(), httptest.NewRequest("", "/", nil))
+		}()
+
+		select {
+		case <-done:
+			t.Fatal("received done too soon")
+		case <-time.After(500 * time.Millisecond):
+			// Do nothing
+		}
+	})
+}
