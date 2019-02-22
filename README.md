27ba1fd166da4cffecc972511b02a7034152fa73
Markus Wüstenberg <markus@wustenberg.dk>
2019-02-22 12:41:59 +0100

    Add Add() function and Middleware type

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # http-middleware
-Useful idiomatic HTTP middleware compatible with net/http.
+
+Useful idiomatic HTTP middleware compatible with `net/http`.
+
+Thanks to [Writing middleware in #golang and how Go makes it so much fun](https://medium.com/@matryer/writing-middleware-in-golang-and-how-go-makes-it-so-much-fun-4375c1246e81) for inspiration.
---
+++ middleware_test.go
@@ -0,0 +1,59 @@
+package middleware_test
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"middleware"
+)
+
+func TestAdd(t *testing.T) {
+	t.Run("adds middleware called from right to left", func(t *testing.T) {
+		var h http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := io.WriteString(w, "hat")
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+
+		calledFirst := func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_, err := io.WriteString(w, "po")
+				if err != nil {
+					t.Fatal(err)
+				}
+				h.ServeHTTP(w, r)
+			})
+		}
+
+		calledSecond := func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_, err := io.WriteString(w, "ny")
+				if err != nil {
+					t.Fatal(err)
+				}
+				h.ServeHTTP(w, r)
+			})
+		}
+
+		h = middleware.Add(h, calledSecond, calledFirst)
+
+		recorder := httptest.NewRecorder()
+		request := httptest.NewRequest("GET", "/", nil)
+		h.ServeHTTP(recorder, request)
+
+		result := recorder.Result()
+		body, err := ioutil.ReadAll(result.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if "ponyhat" != string(body) {
+			t.Fatal("got unexpected body", string(body))
+		}
+	})
+}
+
