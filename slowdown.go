d6b662fea7036d030ef354a1abde86a53387f078
Markus Wüstenberg <markus@wustenberg.dk>
2019-02-22 13:02:08 +0100

    Add Slowdown middleare
    Useful for development.

---
+++ slowdown.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+)
+
+// Slowdown the call to the next handler by the given duration.
+func Slowdown(by time.Duration) Middleware {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(by)
+			h.ServeHTTP(w, r)
+		})
+	}
+}
