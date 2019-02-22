0438a3f4a425fb9d209b676adb832d42f84c5145
Markus Wüstenberg <markus@wustenberg.dk>
2019-02-22 15:15:28 +0100

    Add Timeout middleware (#8)
    Also adjust timing in slowdown test.

--- middleware/timeout_test.go
+++ middleware/timeout_test.go
@@ -9,23 +9,25 @@ import (
 	"github.com/wustenberg/http/middleware"
 )
 
-func TestSlowdown(t *testing.T) {
-	t.Run("slows down handling", func(t *testing.T) {
-		done := make(chan bool, 1)
+func TestTimeout(t *testing.T) {
+	t.Run("times out request after given duration", func(t *testing.T) {
+		cancelled := make(chan bool, 1)
 
 		h := middleware.Add(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			done <- true
-		}), middleware.Slowdown(time.Second))
+			ctx := r.Context()
+			<-ctx.Done()
+			cancelled <- true
+		}), middleware.Timeout(100 * time.Millisecond))
 
 		go func() {
 			h.ServeHTTP(httptest.NewRecorder(), httptest.NewRequest("", "/", nil))
 		}()
 
 		select {
-		case <-done:
-			t.Fatal("received done too soon")
-		case <-time.After(100 * time.Millisecond):
+		case <-cancelled:
 			// Do nothing
+		case <-time.After(200 * time.Millisecond):
+			t.Fatal("context not cancelled")
 		}
 	})
 }
